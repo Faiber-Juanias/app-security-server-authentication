73c4a8c95c2006dbe058858c38ac0c5e9e156277
fjuanias <faiberjuanias001@gmail.com>
2023-09-21 16:54:49 -0500

    Initial commit

---
+++ src/main/java/com/security/appsecurityserverauthentication/AppSecurityServerAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.security.appsecurityserverauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityServerAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSecurityServerAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/security/appsecurityserverauthentication/AppSecurityServerAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.appsecurityserverauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityServerAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
